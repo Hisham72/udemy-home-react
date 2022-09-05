5630ea75bcaa3963829e7773cc274d61c67c135a
Hisham Gamal <hishamgamal5@gmail.com>
2022-09-05 19:46:21 +0200

    add ratings

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,6 +8,15 @@ function Card({data}) {
         <img className={styles.cardImg} src={data.image} alt="python" />
         <h4 className={styles.cardHead}>{data.title}</h4>
         <span className={styles.author}>{data.instructor}</span>
+        <section className={styles.rating}>
+          <span className={styles.ratingNum}>{data.rating}</span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star"></span>
+          <span className="fa fa-star-half-o"></span>
+          <span className={styles.studentsNum}>({data.students})</span>
+        </section>
         <section>
             <span className={styles.current}>E£{data.price}</span>
         </section>
